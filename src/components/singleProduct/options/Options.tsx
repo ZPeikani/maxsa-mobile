633939c995ee18a48c71e4e62df99fddbb3381f9
zahra <z.peikani23@gmail.com>
2024-06-16 22:17:35 +0330

    edit tab in single product

--- src/components/singleProduct/options/Options.tsx
+++ src/components/singleProduct/options/Options.tsx
@@ -1,74 +1,62 @@
-import { Box, Button, Divider } from "@mui/material";
-import { useState } from "react";
-import TechnicalDetails from "./technicalDetaile";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import React from 'react';
+import TechnicalDetails from './technicalDetaile';
 
-export default function Options() {
-  const [activeButton, setActiveButton] = useState<any>("");
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <Box>
-      <Box display="flex" alignItems="center" gap={3}>
-        <Box>
-          <Button
-            variant="text"
-            sx={{
-              fontWeight: "lighter",
-              fontSize: "1.125",
-              textTransform: "none",
-            }}
-            onClick={() =>
-              setActiveButton(activeButton === "details" ? null : "details")
-            }
-            color={activeButton === "details" ? "primary" : "inherit"}
-          >
-            Technical Details
-          </Button>
-          {activeButton === "details" ? (
-            <Divider className="bg-blue-300" />
-          ) : null}
-        </Box>
-        <Box>
-          <Button
-            variant="text"
-            sx={{
-              fontWeight: "lighter",
-              fontSize: "1.125",
-              textTransform: "none",
-            }}
-            onClick={() => {
-              setActiveButton(activeButton === "similar" ? null : "similar");
-            }}
-            color={activeButton === "similar" ? "primary" : "inherit"}
-          >
-            Similar Products
-          </Button>
-          {activeButton === "similar" ? (
-            <Divider className="bg-blue-300" />
-          ) : null}
-        </Box>
-        <Box>
-          <Button
-            variant="text"
-            sx={{
-              fontWeight: "lighter",
-              fontSize: "1.125",
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
 
-              textTransform: "none",
-            }}
-            onClick={() => {
-              setActiveButton(activeButton === "comment" ? null : "comment");
-            }}
-            color={activeButton === "comment" ? "primary" : "inherit"}
-          >
-            Comments
-          </Button>
-          {activeButton === "comment" ? (
-            <Divider className="bg-blue-300" />
-          ) : null}
-        </Box>
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label=" Technical Details" {...a11yProps(0)} sx={{fontSize:"18px",fontWeight:"light",  textTransform: "none"}} />
+          <Tab label="Similar Products" {...a11yProps(1)} sx={{fontSize:"18px",fontWeight:"light",  textTransform: "none"}} />
+          <Tab label="Comments" {...a11yProps(2)} sx={{fontSize:"18px",fontWeight:"light",  textTransform: "none"}} />
+        </Tabs>
       </Box>
-      <Divider />
-      <TechnicalDetails />
+      <CustomTabPanel value={value} index={0}>
+        <TechnicalDetails/>
+      </CustomTabPanel>
+      {/* <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+      </CustomTabPanel> */}
     </Box>
   );
 }
